a1a66674e2cf8add354cc326e81656a72c8ee983
Akshat <akshat.pande@outlook.com>
2024-05-27 01:52:14 +0100

    Google Sheets

--- App.py
+++ App.py
@@ -1,10 +1,29 @@
 import streamlit as st
+import pandas as pd
+import gspread
+import numpy as np
+from oauth2client.service_account import ServiceAccountCredentials
+import time
+from datetime import datetime
+import json
 
-    # Initialize connection.
-conn = st.connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * FROM collection;', ttl=600)
+g_credentials = st.secrets["google_auth"]
+cred = dict(g_credentials) 
+# Google Sheets Authentication
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_dict( cred, scope)
+gc = gspread.authorize(credentials)
+
+
+# Load Google Sheet data
+sheet_url = cred.get("spreadsheet")
+spreadsheet = gc.open_by_url(sheet_url)
+worksheet = spreadsheet.worksheet("stash")
+data = worksheet.get_all_records()
+df = pd.DataFrame(data)
+
+
 
 # Print results.
 
@@ -62,6 +81,17 @@ if cat=='Resource':
 if cat=='LLM':
     for item in df.itertuples():
         if item.category=='LLM':
+            col1, col2 = st.columns([4,1])
+            with col1:
+                with st.expander(item.title + ' ----- [' + item.category + ']'):
+                    st.write(item.information)
+            with col2:
+                st.link_button('Link', item.url)
+
+
+if cat=='Courses':
+    for item in df.itertuples():
+        if item.category=='Courses':
             col1, col2 = st.columns([4,1])
             with col1:
                 with st.expander(item.title + ' ----- [' + item.category + ']'):
